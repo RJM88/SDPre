e7406427b298e7e393664db634ecd3f029156515
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-07-02 13:04:30 -0600

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1{
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 22.2;
+    num2 = 43.4;
+    num3 = 10.9;
+    average = ((num1 + num2 + num3)/3);
+    System.out.println(average);
+  }
+}
