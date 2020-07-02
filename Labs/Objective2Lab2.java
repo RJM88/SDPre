1128899e72d457f8bfee711270801e7ffb47f576
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-07-01 22:08:07 -0600

    complete objective2lab2

--- Labs/Objective2Lab2.java
+++ Labs/Objective2Lab2.java
@@ -2,7 +2,7 @@ public class Objective2Lab2 {
   public static void main(String[] args) {
     String name = "Ryan Miskoviak";
     int age = 32;
-    String state = "Wisconsin" ;
+    String state = "Wisconsin";
     System.out.println("Name: " + name + " Age: " + age + " State: " + state);
   }
 }
