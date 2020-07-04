6c3fdfd984dc23f919ef1ec118922ec6c111b360
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-07-03 22:57:30 -0600

    complete objective3lab2

--- Labs/Objective3Lab2.java
+++ Labs/Objective3Lab2.java
@@ -1,6 +1,6 @@
 public class Objective3Lab2 {
   public static void main(String[] args) {
-    double side1 = 10;
+    double side1 = 20;
     double side2 = 8;
     double hypotenuse = ((side1 * side1) + (side2 * side2));
     hypotenuse = Math.sqrt(hypotenuse);
