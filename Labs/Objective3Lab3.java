4e6b0cb7b302c6ef9bbb4a94665dd29316e36967
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-07-03 23:04:11 -0600

    complete objective3lab3

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -4,7 +4,7 @@ public class Objective3Lab3 {
     first = 1 + (int)(Math.random() *6);
     second = 1 + (int)(Math.random() *6);
     third = 1 + (int)(Math.random() *6);
-    answer = first + second + third;
+    answer = first + second + third + first;
     System.out.println(first + " + " + second + " + " + third + " = " + answer);
   }
 }
