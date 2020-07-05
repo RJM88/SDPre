72e6f03c44cbc33eab94bed528f0a2af800b82e0
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-07-04 22:01:03 -0600

    complete objective4lab3

--- Labs/Objective4Lab3.java
+++ Labs/Objective4Lab3.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class Objective4Lab3 {
   public static void main(String[] args) {
 
+    // Setting variables.
     int birthYear, age, currentYear;
 
+    // Initializing variables.
     birthYear = 0;
     currentYear = 2020;
 
+    // Asking for users age.
     Scanner input = new Scanner(System.in);
     System.out.println("How old are you?");
     age = input.nextInt();
 
+    // Calculating users age
     birthYear = currentYear - age;
 
+    // Printing out users age.
     System.out.println("You were born in " + birthYear + ".");
   }
 }
