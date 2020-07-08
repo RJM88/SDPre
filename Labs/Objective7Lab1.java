9ed50c5b595c1e545690a868cd99a3dc87c19f79
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-07-08 13:13:43 -0600

    complete objective7lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,13 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+
+    //Setting variables.
+    int counter = 10;
+
+    //Creating a while loop. Printing counter variable if greater then 0.
+    while(counter > 0) {
+      System.out.println(counter);
+      counter = counter - 1;
+    }
+  }
+}
