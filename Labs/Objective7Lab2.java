f6fb5757a25f7f168d869a7ae781967c28c508b5
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-07-08 13:21:43 -0600

    complete objective7lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,13 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+    //Setting variables.
+    int counter = 0;
+
+    //Creating a while loop.
+    //Printing counter variable if less then or equal to 20
+    while(counter <= 20) {
+      System.out.println(counter);
+      counter = counter + 1;
+    }
+  }
+}
