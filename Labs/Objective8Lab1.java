72c37f5e330b7ae4156fcde451711a6a2abd22b7
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-07-09 08:49:05 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,17 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    //For loop creates the variable.
+    //Prints the number, adds one then runs again.
+    //Continues until it greater then or equal to 10.
+    for (int num = 1; num <= 10; num++) {
+      System.out.println(num);
+    }
+    //For loop creates the variable.
+    //Prints the number, subtracts one then runs again.
+    //Continues until it less then or equal to 1.
+    for (int num = 10; num >=1; num --) {
+      System.out.println(num);
+    }
+  }
+}
