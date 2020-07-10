f35c7e9f9f8fc63ad19b1d2de0750c3c355eba35
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-07-09 21:53:20 -0600

    complete objective9lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,10 @@
+public class Objective9Lab1 {
+  public static void main(String[] args) {
+  //Calling printName method.
+    printName();
+  }
+  //Method will print "Hello Doug".
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
